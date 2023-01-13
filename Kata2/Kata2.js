449f16619401d979c22be5f36c7a851a56b7275b
Melhed <82815800+Melhed@users.noreply.github.com>
2023-01-13 09:58:53 +0100

    Fix achieved via kata walkthrough

--- Kata2/Kata2.js
+++ Kata2/Kata2.js
@@ -1,28 +1,16 @@
-let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let tempNumArr = [];
 let tempSum = 0;
 
-
 function consecutiveSum(num) {
+    for(let i = 1; i < (num / 2) + 1; i++) {
+        tempNumArr = [];
+        tempSum = 0;
 
-    for(let i = 0; i < numArr.length; i++) {
-        tempNumArr.push(numArr[i]);
-
-        for(let j = i+1; j < numArr.length; j++) {
-            tempSum = calculateSum();
-            if(tempSum + numArr[j] === num) {
-                tempNumArr.push(numArr[j]);
+        for(let j = i; j < num / 2; j++) {
+            tempNumArr.push(j);
+            if(calculateSum() === num) {
                 console.log(tempNumArr);
                 return true;
-            }
-
-            if(tempSum + numArr[j] < num) {
-                tempNumArr.push(numArr[j]);
-            }
-            
-            if(tempSum + numArr[j] > num) {
-                tempNumArr = [];
-                tempSum = 0;
             }
         }
     }
@@ -36,4 +24,4 @@ function calculateSum() {
     return tempSum;
 }
 
-consecutiveSum(35);
+consecutiveSum(34);
